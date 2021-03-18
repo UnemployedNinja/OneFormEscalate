9cbad7569e9ef7f7b7df0b36b219354ed8a9aafc
Shawn <phi13025@byui.edu>
2021-03-18 12:01:52 -0600

    Added more emails. Almost done!

--- src/main/java/Escalate.java
+++ src/main/java/Escalate.java
@@ -1,11 +1,3 @@
-import td.api.CustomAttribute;
-import td.api.TDException;
-import td.api.TeamDynamix;
-import td.api.Ticket;
-
-import java.rmi.server.UID;
-import java.util.ArrayList;
-
 public class Escalate {
 
     /**
@@ -27,150 +19,272 @@ public class Escalate {
             // One From -> General
             case 31877: // Alumni Office
                 email = "alumni@byui.edu";
+                break;
             case 31878: // Bowling/Skating
                 email = "roseh@byui.edu";
+                break;
             case 31879: // BSC (about the Support Center)
                 email = null;
+                break;
             case 31881: // Catering
                 email = "catering@byui.edu";
+                break;
             case 31882: // Center Stage
                 email = "centerstage@byui.edu";
+                break;
             case 31880: // Continuing Endorsement
                 email = "sho@byui.edu";
+                break;
             case 34472: // Counseling Center
                 email = "counselingsec@byui.edu";
+                break;
             case 31883: // Covid-19 (Coronavirus) Concerns
                 email = null;
+                break;
             case 31884: // Devotional
                 email = "uroffice@byui.edu";
+                break;
             case 31885: // Direct to Employee
                 email = null;
+                break;
             case 31886: // Disability Services
                 email = "disabilityservices@byui.edu";
+                break;
             case 31887: // Donations
                 email = "philanthropies@churchofjesuschrist.org";
+                break;
             case 31888: // Event Services
                 email = "eventcoordinator@byui.edu";
+                break;
             case 31889: // Executive Offices
                 email = null;
+                break;
             case 31890: // Express Print & Copy (Small and Quick Jobs)
                 email = "print@byui.edu";
+                break;
             case 36079: // Faculty Technology Center
                 email = "FTC@byui.edu";
+                break;
             case 31919: // Fitness/Wellness Center
                 email = "wellness@byui.edu";
+                break;
             case 31891: // Food Services
                 email = "catering@byui.edu";
+                break;
             case 31892: // Gave Directions To Location
                 email = null;
+                break;
             case 34471: // Health Center
                 email = "healthcenterbilling@byui.edu";
+                break;
             case 31895: // Hours of operations
                 email = null;
+                break;
             case 31893: // Honor Office
                 email = "sho@byui.edu";
+                break;
             case 31894: // Housing Office
                 email = "housing@byui.edu";
+                break;
             case 33587: // HR: Employment Verification
                 email = null;
+                break;
             case 31896: // HR: Human Resources
                 email = "humanresources@byui.edu";
+                break;
             case 31902: // HR: Online Employment & Scheduling
                 email = "onlinehr@byui.edu";
+                break;
             case 33585: // HR: Student Employment
                 email = "humanresources@byui.edu";
+                break;
             case 33588: // Tuition Waiver
                 email = "benefits@byui.edu";
+                break;
             case 33586: // HR: Workday
                 email = "workday@byui.edu";
+                break;
             case 31897: // International Services
+                email = "international@byui.edu";
+                break;
             case 31898: // International Study Program
+                email = "traveladmin@byui.edu";
+                break;
             case 31900: // Lost and Found Questions (Not Transferred)
                 email = "NULL";
+                break;
             case 31901: // myBYUI Student Portal (New)
+                email = "nicholsr@byui.edu";
+                break;
             case 31903: // ORC (Outdoor Resource Center)
+                email = "orc@byui.edu";
+                break;
+                // TODO: Find email
             case 34757: // Pathway Connect Issues
+                email = "https://www.byupathway.org/help/contact-us";
+                break;
             case 31805: // Planetarium
+                email = "scheduling@byui.edu";
+                break;
             case 31904: // Print & Copy Services
+                email = "print@byui.edu";
+                break;
             case 31905: // Radio/KBYi
+                email = "isleb@byui.edu";
+                break;
             case 31906: // ROTC
+                email = "hamiltonau@byui.edu";
+                break;
             case 31907: // Safety & Security
+                email = "publicsafety@byui.edu";
+                break;
             case 31908: // Scheduling Office
                 email = "scheduling@byui.edu";
+                break;
+                // TODO: Scroll Office
             case 33593: // Scroll Office
+                break;
             case 31909: // Sexual Misconduct
+                email = "titleix@byui.edu";
+                break;
             case 31910: // Stores & Receiving
+                email = "spauldingbr@byui.edu";
+                break;
             case 31911: // Student Activities
+                email = "activities@byui.edu";
+                break;
             case 31912: // Student Support
+                email = "studentsupport@byui.edu";
+                break;
             case 31913: // Surplus Sale
+                email = "surplussales@byui.edu";
+                break;
             case 31915: // Ticket Office
+                email = "ticketoffice@byui.edu";
+                break;
             case 31916: // Travel Office
+                email = "traveloffice@byui.edu";
+                break;
             case 31917: // University Relations
+                email = "uroffice@byui.edu";
+                break;
             case 31918: // University Texting
-            case 31920: // Zimride
+                email = "service@byuistore.com";
+                break;
 
             // One Form -> Academic
             case 31788: // Art Dept.
                 email = "art_dept@byui.edu";
+                break;
             case 31789: // Automotive Dept.
                 email = "automotivedept@byui.edu";
+                break;
             case 31797: // Career Services
                 email = "careerservices@byui.edu";
+                break;
             case 31790: // College of Agriculture and Life Sciences (NOT ADVISING)
                 email = "stuartra@byui.edu";
+                break;
             case 31791: // College of Business and Communications (NOT ADVISING)
                 email = "buscom@byui.edu";
+                break;
             case 31792: // College of Education and Human Development (NOT ADVISING)
                 email = "chidesterk@byui.edu";
+                break;
             case 31793: // College of Language & Letters (NOT ADVISING)
                 email = "gohrm@byui.edu";
+                break;
             case 31794: // College of Performing & Visual Arts (NOT ADVISING)
                 email = "collegePVA@byui.edu";
+                break;
             case 31795: // College of Physical Sciences & Engineering (NOT ADVISING)
                 email = "belnapj@byui.edu";
+                break;
             case 31808: // Dean of Students
                 email = "dos@byui.edu";
+                break;
             case 31798: // Field Services (Student Teaching Placement)
                 email = "fieldservices@byui.edu";
+                break;
             case 34726: // Instructor Complaint or Grievance
+                email = "thuesonki@byui.edu";
+                break;
             case 31799: // Internship Questions
                 email = "internships@byui.edu";
+                break;
             case 31800: // Math Dept.
-            case 31801: // Nursing-On Campus (pre-Licensre BSC)
+                email = "gohrj@byui.edu";
+                break;
+            case 31801: // Nursing-On Campus (pre-License BSC)
+                email = "potterst@byui.edu";
+                break;
             case 31802: // Nursing-Online (RN to BSN)
+                email = "rnbsn@byui.edu";
+                break;
             case 31803: // Office of General Education (NOT ADVISING)
+                email = "klinglerg@byui.edu";
+                break;
             case 31804: // Office of Interdisciplinary Studies (NOT ADVISING)
+                email = "interdisciplinarystudies@byui.edu";
+                break;
             case 36152: // Positive Instructor Feedback
+                email = null;
+                break;
             case 34768: // Proctorio
+                email = "remoteproctoring@byui.edu";
+                break;
             case 31806: // Religion Dept.
+                email = "linfordk@byui.edu";
+                break;
             case 31914: // Testing Center
+                email = "testcenter@byui.edu";
+                break;
             case 31807: // Tutoring (Academic Support Center)
+                email = "tutoringcenter@byui.edu";
+                break;
 
             // One Form -> Facilites
             case 31952: // Access Control/Locks & Keys
                 email = "accesscontrol@byui.edu";
+                break;
             case 31945: // Custodial
                 email = "facilitiesserv@byui.edu";
+                break;
             case 31946: // Facilities/Grounds
                 email = "facilitiesserv@byui.edu";
+                break;
             case 31947: // Fleet Services
                 email = "hiltonk@byui.edu";
+                break;
             case 33595: // Gardens
                 email = null;
+                break;
             case 31948: // Hart Equipment Room/ Pool/ Fitness Center
                 email = "equipmentroom@byui.edu";
+                break;
             case 31899: // Library
                 email = "ill@byui.edu";
+                break;
             case 33598: // Livestock Center
+                email = "gouldingr@byui.edu";
+                break;
             case 31949: // Mail Services
-            case 31950: // Maps
+                email = "mailservices@byui.edu";
+                break;
             case 33596: // Orchard
+                email = "pruittm@byui.edu";
+                break;
             case 31951: // Parking Services
+                email = "parkingservices@byui.edu";
+                break;
             case 33597: // Plant Shop
+                email = "pruittm@byui.edu";
+                break;
 
             // Other
             default:
                 email = null;
+                break;
         }
 
         return email;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,8 @@
-import td.api.TDException;
-import td.api.TeamDynamix;
-import td.api.Ticket;
-
 public class Main {
 
-    public static final int TAG_VALUE_ID = 13221;
+    public static final int TAG_VALUE_ID = 31877;
 
     public static void main(String[] args) {
-
-//        Ticket ticket = new Ticket();
-//        ticket.setResponsibleUid(String);
 
         Escalate escalate = new Escalate();
         String email = escalate.oneSwitchToRuleThemAll(TAG_VALUE_ID);
